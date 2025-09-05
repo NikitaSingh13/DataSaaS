a37474a83111ead0b8e172ede78e24133baee133
nikita <nikitasinghak257@gmail.com>
2025-09-05 18:30:47 +0530

    css updated

--- DataSaaS/settings.py
+++ DataSaaS/settings.py
@@ -215,3 +215,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'home'
+
+
+#for production
+TAILWIND_DEV_MODE = False  # False for production, True for dev
